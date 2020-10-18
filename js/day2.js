1458d1f3c09ad4fb026238032c1fd790d5226513
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-18 19:19:46 +0530

    Day 2 - Part 2

--- js/day2.js
+++ js/day2.js
@@ -1,51 +1,57 @@
 var WIDTH = 800;
 var HEIGHT = 800;
 
-var phase = 0;
-var lightBlue = "#adf";
-var darkBlue = "#469";
-var rectangles = [];
-var colors = [lightBlue, darkBlue];
-var oscillation = 0.5;
-
 class Rectangle {
   constructor(options) {
-    this.x = 400
-    this.y = options.height
+    this.x = options.x
+    this.y = options.y
     this.phase = options.index * 10
     this.rotation = options.rotation
     this.direction = options.direction
     this.color = options.color
   }
+
+  update(dt) {
+    this.phase += oscillation * 360 * dt / 1000;
+  }
+
+  draw() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.rotation * this.direction * cos(this.phase) * cos(this.phase) * cos(this.phase));
+    fill(this.color);
+    rect(-1000, -50, 2000, rectHeight * 2);
+    pop();
+  }
 }
 
-// var rectangles = [
-//   { x: 400, y: 50,  rotation: rotation,  color: lightBlue },
-//   { x: 400, y: 150, rotation: -rotation,  color: darkBlue },
-//   ...
-// ]
+var phase = 0;
+var lightBlue = "#adf";
+var darkBlue = "#469";
+var rectangles = [];
+var colors = [lightBlue, darkBlue];
+var oscillation = 0.2;
 var rectHeight = 40;
 var rotation = 3;
 var color = 0;
 var direction = 1;
 var index = 1;
-for (var height = -150; height < 1000; height += rectHeight) {
-  rectangles.push({
+
+for (var y = -150; y < 1000; y += rectHeight) {
+  rectangles.push(new Rectangle({
     index: index,
     x: 400,
-    y: height,
+    y: y,
     phase: index * 10,
     rotation: rotation,
     direction: direction,
     color: colors[color]
-  });
+  }));
 
   index += 1;
   direction *= -1;
   color = (color + 1) % colors.length;
 }
-
-
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
@@ -57,12 +63,7 @@ function draw() {
   background(255);
 
   rectangles.forEach(function(rectangle) {
-    push();
-    translate(rectangle.x, rectangle.y);
-    rotate(rectangle.rotation * rectangle.direction * cos(rectangle.phase));
-    fill(rectangle.color);
-    rect(-1000, -50, 2000, rectHeight * 2);
-    pop();
-    rectangle.phase += oscillation * 360 * deltaTime / 1000;
+    rectangle.draw();
+    rectangle.update(deltaTime);
   });
 }
