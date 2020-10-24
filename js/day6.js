0ea52ad40ab1b74ebb84aab755c47613cfed2d24
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-24 23:09:57 +0530

    Day 6

--- js/day6.js
+++ js/day6.js
@@ -2,11 +2,8 @@ var WIDTH = 800;
 var HEIGHT = 800;
 
 var size = 50;
-var base = 22.5;
-var offset = 22.5;
-var variation = 2.5;
+var offset = 20;
 var phase = 0;
-var oscillation = 60;
 var bg = "#aa2211";
 var lightRed = "#ffdd7722";
 var darkRed = "#aa221122";
@@ -15,22 +12,25 @@ function setup() {
   createCanvas(WIDTH, HEIGHT);
   noStroke();
   angleMode(DEGREES);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(darkRed);
-  for(var x = -50, col = 0; x < WIDTH; x += offset, col++) {
-    for(var y = -50, row = 0; y < HEIGHT; y += offset, row++) {
+  for(var x = -50, col = 0; x < WIDTH + 2*size; x += offset, col++) {
+    for(var y = -50, row = 0; y < HEIGHT + 2*size; y += offset, row++) {
       var color = ((col + row) % 2 === 0) ? lightRed : darkRed;
-
+      var localPhase = phase - (row + col);
       push();
       translate(x, y);
+      if (localPhase > 360 && int(localPhase / 90) % 2 == 0) {
+        rotate(localPhase);
+      }
       fill(color);
       rect(0, 0, size, size);
       pop();
     }
   }
 
-  phase += oscillation * deltaTime / 1000;
-  offset = base + variation * sin(phase);
+  phase += 36 * deltaTime / 1000;
 }
